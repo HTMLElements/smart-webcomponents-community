f9aae41005bc58d5eeadcaadd38b528f017ec4f1
bmarkov <boikom@jqwidgets.com>
2018-04-12 17:17:10 +0300

    Update karma-minified.conf.js

--- karma-minified.conf.js
+++ karma-minified.conf.js
@@ -37,7 +37,7 @@ module.exports = function (config) {
         ],
 
         // list of files to exclude
-        exclude: ['source-minified/smart-elements.js'],
+        exclude: ['source-minified/smart.elements.js'],
 
         // preprocess matching files before serving them to the browser
         // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
