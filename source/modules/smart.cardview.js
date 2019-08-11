48b7892c4e5484a9a7b59a7131a24109dad7a7ff
bmarkov <boikom@jqwidgets.com>
2019-08-11 23:34:53 +0300

    Add files via upload

---
+++ source/modules/smart.cardview.js
@@ -0,0 +1,21 @@
+import "../smart.element.js";
+import "../smart.button.js";
+import "../smart.scrollbar.js";
+import "../smart.data.js";
+import "../smart.date.js";
+import "../smart.listbox.js";
+import "../smart.input.js";
+import "../smart.dropdownlist.js";
+import "../smart.calendar.js";
+import "../smart.datetimepicker.js";
+import "../smart.switchbutton.js";
+import "../smart.cardview.js";
+
+export class smartCardView extends Smart.Component {
+	get name() {
+		return 'smartCardView';
+	}		
+}
+
+export class smartDataAdapter extends Smart.DataAdapter {
+}
