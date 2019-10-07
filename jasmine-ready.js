818b38521b591a7871aa10a655bde6e7e61ece94
bmarkov <boikom@jqwidgets.com>
2019-10-07 10:06:18 +0300

    Delete jasmine-ready.js

--- jasmine-ready.js
+++
@@ -1,8 +0,0 @@
-﻿window.__karma__.start = (function (originalStartFn) {
-    return function () {
-        var args = arguments
-        jQuery(function () {
-            originalStartFn.apply(null, args);
-        });
-    };
-}(window.__karma__.start));
