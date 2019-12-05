024b61ef2d5c617017d5f3384218d823e510d81a
jqwidgets <info@jqwidgets.com>
2019-12-05 18:40:14 +0200

    5.1.0

---
+++ source-community/smart.grid.group.js
@@ -0,0 +1,20 @@
+
+/* Smart HTML Elements v5.1.0 (2019-Dec) 
+Copyright (c) 2011-2019 jQWidgets. 
+License: https://htmlelements.com/license/ */
+
+Smart.Utilities.Assign("Grid.Group",class
+{
+	addGroup(dataField) {
+		alert('Smart.Grid Pro Version is required for this feature! You can get it from: https://www.htmlelements.com/license/');	
+    }
+
+    removeGroup(dataField) {
+		alert('Smart.Grid Pro Version is required for this feature! You can get it from: https://www.htmlelements.com/license/');	
+    }
+
+    clearGroups() {
+		alert('Smart.Grid Pro Version is required for this feature! You can get it from: https://www.htmlelements.com/license/');	
+    }
+});
+
