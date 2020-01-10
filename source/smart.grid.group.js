1df5a285e40b156945632f1ab674fd4f7a6743c9
jqwidgets <info@jqwidgets.com>
2020-01-10 08:18:34 +0200

    6.0.0

--- source/smart.grid.group.js
+++ source/smart.grid.group.js
@@ -1,20 +1,6 @@
 
-/* Smart HTML Elements v5.1.0 (2019-Dec) 
-Copyright (c) 2011-2019 jQWidgets. 
+/* Smart HTML Elements v6.0.0 (2020-Jan) 
+Copyright (c) 2011-2020 jQWidgets. 
 License: https://htmlelements.com/license/ */
 
-Smart.Utilities.Assign("Grid.Group",class
-{
-	addGroup(dataField) {
-		alert('Smart.Grid Pro Version is required for this feature! You can get it from: https://www.htmlelements.com/license/');	
-    }
-
-    removeGroup(dataField) {
-		alert('Smart.Grid Pro Version is required for this feature! You can get it from: https://www.htmlelements.com/license/');	
-    }
-
-    clearGroups() {
-		alert('Smart.Grid Pro Version is required for this feature! You can get it from: https://www.htmlelements.com/license/');	
-    }
-});
-
+Smart.Utilities.Assign("Grid.Group",class{addGroup(a){const b=this;if(b.dataSource&&b.grouping.enabled){const c=b.dataSource.groupBy.indexOf(a);-1===c&&(b.dataSource.groupBy.push(a),b.refresh(!0),b.refreshFilters())}}removeGroup(a){const b=this;if(b.dataSource&&b.grouping.enabled){const c=b.dataSource.groupBy.indexOf(a);0<=c&&(b.dataSource.groupBy.splice(c,1),b.refresh(!0),b.refreshFilters())}}clearGroups(){const a=this;a.dataSource&&a.grouping.enabled&&(a.dataSource.clearGroup(),a.refresh(!0),a.refreshFilters())}});
