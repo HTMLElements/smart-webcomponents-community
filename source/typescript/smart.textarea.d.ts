bf33c270f5a682616c9c97983a2294ec464e1ec8
bmarkov <boikom@jqwidgets.com>
2020-06-29 10:41:57 +0300

    Smart UI 7.7.0

--- source/typescript/smart.textarea.d.ts
+++ source/typescript/smart.textarea.d.ts
@@ -1,64 +1,61 @@
-import  {BaseElement} from "./smart.element"
+import  {BaseElement, Animation} from "./smart.element"
 
-/**
- Input specifies an input field where the user can enter data. Auto-complete options are displayed for easier input.
-*/
-export interface Input extends BaseElement {
+export interface TextAreaProperties {
   /**
    * Sets or gets the animation mode. Animation is disabled when the property is set to 'none'
    * Default value: advanced
    */
-  animation: "none" | "simple" | "advanced";
+  animation?: Animation;
   /**
    * Determines the delay before the drop down opens to show the matches from the auto complete operation. The delay is measured in miliseconds.
    * Default value: 100
    */
-  autoCompleteDelay: number;
+  autoCompleteDelay?: number;
   /**
    * Determines the data source that will be loaded to the Input. The dataSource can be an array of strings/numbers or objects where the attributes represent the properties of a List Item. For example label, value. It can also be a callback that returns an Array of items as previously described.
    * Default value: null
    */
-  dataSource: any;
+  dataSource?: any;
   /**
    * Enables or disables the element.
    * Default value: false
    */
-  disabled: boolean;
+  disabled?: boolean;
   /**
    * Determines the position of the drop down button.
    * Default value: right
    */
-  dropDownButtonPosition: "none" | "left" | "right";
+  dropDownButtonPosition?: DropDownButtonPosition;
   /**
    * Sets the height of the drop down. By default it's set to an empty string. In this case the height of the drop down is controlled by a CSS variable.
    * Default value: 
    */
-  dropDownHeight: string | number;
+  dropDownHeight?: string | number;
   /**
    * Sets the width of the drop down. By default it's set to an empty string. In this case the width of the drop down is controlled by a CSS variable.
    * Default value: 
    */
-  dropDownWidth: string | number;
+  dropDownWidth?: string | number;
   /**
    * Sets the purpose of the input and what, if any, permission the user agent has to provide automated assistance in filling out the element's input when in a form, as well as guidance to the browser as to the type of information expected in the element. This value corresponds to the standard HTML autocomplete attribute and can be set to values such as 'on', 'name', 'organization', 'street-address', etc.
    * Default value: "off"
    */
-  inputPurpose: string;
+  inputPurpose?: string;
   /**
    * Determines the maximum number of matched items that should be visible inside the drop down as a result of a new autoComplete query. By default the maximum number of 8 items can be displayed inside the drop down.
    * Default value: 8
    */
-  items: number;
+  items?: number;
   /**
    * Sets or gets the language. Used in conjunction with the property messages. 
    * Default value: "en"
    */
-  locale: string;
+  locale?: string;
   /**
    * Callback used to customize the format of the messages that are returned from the Localization Module.
    * Default value: null
    */
-  localizeFormatFunction: any;
+  localizeFormatFunction?: any;
   /**
    * Sets or gets an object specifying strings used in the widget that can be localized. Used in conjunction with the property locale. 
    * Default value:    * {
@@ -75,86 +72,94 @@ export interface Input extends BaseElement {
    *   }
    * }
    */
-  messages: any;
+  messages?: any;
   /**
    * Determines the minimum number of characters inside the input in order to trigger the autocomplete functionality that will open the drop down and show the matched items.
    * Default value: 1
    */
-  minLength: number;
+  minLength?: number;
   /**
    * Sets or gets the name attribute for the element. Name is used when submiting data inside an HTML form.
    * Default value: ""
    */
-  name: string;
+  name?: string;
   /**
    * Determines whether the drop down is opened or not.
    * Default value: false
    */
-  opened: boolean;
+  opened?: boolean;
   /**
    * Determines the placeholder of the input.
    * Default value: ""
    */
-  placeholder: string;
+  placeholder?: string;
   /**
    * Sets or gets the query that is used to filter the items. Query is used by the autoComplete operation. Empty string means that all items from the data source will be displayed and no filter query is applied.
    * Default value: 
    */
-  query: string | number;
+  query?: string | number;
   /**
    * Determines the auto complete query mode. This property also determines the matching algorithm for the autocomplete operation.
    * Default value: containsIgnoreCase
    */
-  queryMode: "contains" | "containsIgnoreCase" | "doesNotContain" | "doesNotContainIgnoreCase" | "equals" | "equalsIgnoreCase" | "startsWith" | "startsWithIgnoreCase" | "endsWith" | "endsWithIgnoreCase";
+  queryMode?: TextAreaQueryMode;
   /**
    * Determines whether ot not the user can enter text inside the input. if dropDownButtonPosition is set to 'left' or 'right' then readonly determines whether the element acts as a ComboBox or a DropDownList if a dataSource is provided.
    * Default value: false
    */
-  readonly: boolean;
+  readonly?: boolean;
   /**
    * Sets or gets the value indicating whether the element is aligned to support locales using right-to-left fonts.
    * Default value: false
    */
-  rightToLeft: boolean;
+  rightToLeft?: boolean;
   /**
    * Determines whether the items are sorted alphabetically or not
    * Default value: false
    */
-  sorted: boolean;
+  sorted?: boolean;
   /**
    * Determines the sorting algorithm - ascending(asc) or descending(desc) if sort is enabled.
    * Default value: "asc"
    */
-  sortDirection: string;
+  sortDirection?: string;
   /**
    * Determines the theme for the element. Themes define the look of the elements.
    * Default value: ""
    */
-  theme: string;
+  theme?: string;
   /**
    * Determines the input type. Input type determines what input can be entered.
    * Default value: ""
    */
-  type: string;
+  type?: string;
   /**
    * If is set to true, the element cannot be focused.
    * Default value: false
    */
-  unfocusable: boolean;
+  unfocusable?: boolean;
   /**
    * Sets or gets the value of the element.
    * Default value: ""
    */
-  value: string;
-  /** 
+  value?: string;
+}
+/**
+ TextArea specifies a textarea field where the user can enter data. Auto-complete options are displayed for easier input.
+*/
+export interface TextArea extends BaseElement, TextAreaProperties {
+
+  /* Get a member by its name */
+  [name: string]: any;
+  /**
    * This event is triggered when the selection is changed.
-   * @param ev. The custom event. Custom data event was created with: ev.detail(label, oldLabel, oldValue, value)
+	* @param event. The custom event. Custom data event was created with: ev.detail(label, oldLabel, oldValue, value)
    *  label - The label of the new selected item.
    *  oldLabel - The label of the item that was previously selected before the event was triggered.
    *  oldValue - The value of the item that was previously selected before the event was triggered.
    *  value - The value of the new selected item.
    */
-  onchange: ((this: Window, ev: Event) => any) | null;
+  onChange: ((this: any, ev: Event) => any) | null;
   /**
    * Closes the drop down.
    */
@@ -173,9 +178,17 @@ export interface Input extends BaseElement {
   select(): void;
 }
 
-declare global {    
+declare global {
     interface Document {
-        createElement(tagName: "smart-input"): Input;
+        createElement(tagName: "smart-text-area"): TextArea;
+        querySelector(selectors: "smart-text-area"): TextArea | null;
+        querySelectorAll(selectors: "smart-text-area"): NodeListOf<TextArea>;
+        getElementsByTagName(qualifiedName: "smart-text-area"): HTMLCollectionOf<TextArea>;
+        getElementsByName(elementName: "smart-text-area"): NodeListOf<TextArea>;
     }
 }
 
+/**Determines the position of the drop down button. */
+export declare type DropDownButtonPosition = 'none' | 'left' | 'right';
+/**Determines the auto complete query mode. This property also determines the matching algorithm for the autocomplete operation. */
+export declare type TextAreaQueryMode = 'contains' | 'containsIgnoreCase' | 'doesNotContain' | 'doesNotContainIgnoreCase' | 'equals' | 'equalsIgnoreCase' | 'startsWith' | 'startsWithIgnoreCase' | 'endsWith' | 'endsWithIgnoreCase';
